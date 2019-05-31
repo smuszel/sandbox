c695410d3f72c280cff539d1deaba776a35eb425
Stanislaw Muszel <stanislaw.muszel@softwaremind.com>
2019-05-31 14:28:44 +0200

    fix

--- test/main.spec.js
+++ test/main.spec.js
@@ -9,16 +9,16 @@ module.exports = function*() {
     assert(f() === 0);
     //
     yield 'single value 1 evaluates to 1';
-    assert(f(1) === 1);
+    assert(f('1') === 1);
     //
     yield 'single value 0 evaluates to 0';
-    assert(f(0) === 0);
+    assert(f('0') === 0);
     //
     yield 'empty evaluates to 0';
     assert(f('') === 0);
     //
     yield 'adding 2 and 5 evaluates to 7';
-    assert(f('2 7') === 7);
+    assert(f('2 5') === 7);
     //
     yield 'end';
 };
