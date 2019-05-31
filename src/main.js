79d62560786370f470df5a05b19caf911026d774
Stanislaw Muszel <stanislaw.muszel@softwaremind.com>
2019-05-31 14:48:14 +0200

    green4

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 module.exports = text => {
-    return (text || '').split(' ').reduce((acc, x) => acc + +x, 0);
+    return (text || '')
+        .replace(/,/g, ' ')
+        .split(' ')
+        .reduce((acc, x) => acc + +x, 0);
 };
