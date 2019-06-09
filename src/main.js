1e73efe099a558c4aebb9213301379f712d34bfa
smuszel <stanislaw.muszel@gmail.com>
2019-06-09 05:43:20 +0200

    string calc argv

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-const effRun = require('./effRun');
+module.exports = ({ process, console }) => {
+    const str = process.argv.slice(2)[0] || '';
+    const sum = str
+        .replace(/\n/g, ',')
+        .split(',')
+        .reduce((acc, x) => acc + +x, 0);
+    return sum;
+};
--- src/test.js
+++ src/test.js
@@ -1,6 +1,35 @@
 // @vanad
 const t = require('vanad');
+const f = require('./main');
 
-t('abc', t => {
-    t(1, 1);
+const consoleMock = { log: x => x };
+
+const emptyUndef = { argv: ['', '', undefined], console: consoleMock };
+const emptyStr = { argv: ['', '', ''] };
+
+const single1 = { argv: ['', '', '1'] };
+const single101 = { argv: ['', '', '101'] };
+
+const multiple1_3 = { argv: ['', '', '1,3'] };
+const multiple10_20_100 = { argv: ['', '', '10,20,100'] };
+
+const delimNewLine = { argv: ['', '', '10\n20\n100'] };
+
+t('Starts with zero', c => {
+    c(f({ process: emptyStr }), 0);
+    c(f({ process: emptyUndef }), 0);
+});
+
+t('Parses single values', c => {
+    c(f({ process: single1 }), 1);
+    c(f({ process: single101 }), 101);
+});
+
+t('Adds multiple values', c => {
+    c(f({ process: multiple1_3 }), 4);
+    c(f({ process: multiple10_20_100 }), 130);
+});
+
+t('Can use other delimiters', c => {
+    c(f({ process: delimNewLine }), 130);
 });
